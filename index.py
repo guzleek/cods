cbbbb665fce2b582f7d399954581d0beb09a5173
guzl1k <124172337+guzleek@users.noreply.github.com>
2023-03-08 01:44:47 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,43 @@
-a = LL
+from colorama import init
+from colorama import Fore, Back, Style
+import random
+from time import sleep
+from rich.progress import track
 
-print ("a")
+def do_step(step):
+    sleep(random.uniform(0.01, 0.1))
+
+for step in track(range(100), description= "Wait please: "):
+    do_step(step)
+print(Fore.MAGENTA)
+d = int(input("Enter the password for start: "))
+
+while True:
+
+    if d != 1378:
+        print(Fore.RED)
+        print("\nError password: ")
+        print(Fore.MAGENTA)
+        d = int(input("\nRepeat password: "))
+        continue
+    elif d == 1378:
+        print(Fore.GREEN)
+        print("Correct password! ")
+        print(Fore.MAGENTA)
+        print("\nStart programs...\n" + " Load...")
+        break
+    else:
+        print("Error")
+a = float(input("\nEnter the first number: "))
+b = float(input("Enter the second number: "))
+
+quest = input("What the operation ? (+,-): ")
+
+if quest == "+":
+    c = a + b 
+    print("Answer: " + str(c))
+elif quest == "-":
+    c = a - b 
+    print("Answer: " + str(c))
+else:
+    print("Error")
